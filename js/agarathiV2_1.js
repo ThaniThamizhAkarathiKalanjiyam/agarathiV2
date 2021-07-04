a0cafd29de7b0166cdad3a7ee333623c56518420
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2021-07-04 12:30:26 +0530

    ji

--- js/agarathiV2_1.js
+++ js/agarathiV2_1.js
@@ -13,31 +13,60 @@ $(document).ready(function () {
                 }));
         });
 
-       
-		 $.each(tamil_lettersJSON.tam_mey_plus_uyirmey_letters, function (index, value) {
+        $.each(tamil_lettersJSON.tam_mey_plus_uyirmey_letters, function (index, value) {
 
-                    $('#sel_edhugai_letter').append($('<option>', {
-                            value: value,
-                            text: value
-                        }));
-                });
-        setMayankuMeykal();
-    });
-
-    setMayankuMeykal = function () {
-
-     var sel_edhugai_letterVal = $('#sel_edhugai_letter').val()
-
-            if (sel_edhugai_letterVal === "") {
-                $.each(tamil_lettersJSON.tam_last_letters, function (index, value) {
-
-            $('#sel_last_letter').append($('<option>', {
+            $('#sel_edhugai_letter').append($('<option>', {
                     value: value,
                     text: value
                 }));
         });
+        setMayankuMeykal();
+
+        $('#MyTableContainer').jtable({
+
+            //General options comes here
+
+            actions: {
+                listAction: function (postData, jtParams) {
+					var first_letter = $("#sel_first_letter").val()
+					var last_letter = $("#sel_last_letter").val()
+                    var records = tamil_lettersJSON.getGeneratedWords(
+					[first_letter],
+					[last_letter]
+					)
+
+                    return {
+                        "Result": "OK",
+                        "Records": records,
+                        "TotalRecordCount": 2
+                    };
+                }
+            },
+            fields: {
+                word: {
+                    title: "Word"
+                }
+            }
+
+            //Event handlers...
+        })
+
+    });
+
+    setMayankuMeykal = function () {
+
+        var sel_edhugai_letterVal = $('#sel_edhugai_letter').val()
+
+            if (sel_edhugai_letterVal === "") {
+                $.each(tamil_lettersJSON.tam_last_letters, function (index, value) {
+
+                    $('#sel_last_letter').append($('<option>', {
+                            value: value,
+                            text: value
+                        }));
+                });
             } else {
-				$('#sel_last_letter').empty();
+                $('#sel_last_letter').empty();
                 var getMayankuMeyKal = tamil_lettersJSON.getMayankuMey(sel_edhugai_letterVal)
                     $.each(getMayankuMeyKal, function (index, value) {
                         $('#sel_last_letter').append($('<option>', {
@@ -50,9 +79,20 @@ $(document).ready(function () {
     }
 
     $('#sel_edhugai_letter').on("change", function () {
-		
-		setMayankuMeykal()
-		
-	});
+
+        setMayankuMeykal()
+		$('#MyTableContainer').jtable("load");
+
+    });
+	$('#sel_first_letter').on("change", function () {
+
+        $('#MyTableContainer').jtable("load");
+
+    });
+    $('#btnCreateWord').click(function () {
+
+        $('#MyTableContainer').jtable("load");
+
+    });
 
 });
